54ef09e5fa6063c71f50c773b1b1192c702b3ddc
sveinnvi <sveinnvi@gmail.com>
2016-11-03 21:48:47 +0000

    Uppkast af útliti, tekið af codepen

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,50 @@
 'use strict';
 
-var foo = [1, 2, 3];
-var bar = foo.map(function (i) {
-  return i * 2;
-});
+var Expand = function () {
+  var tile = $('.strips__strip');
+  var tileLink = $('.strips__strip > .strip__content');
+  var tileText = tileLink.find('.strip__inner-text');
+  var stripClose = $('.strip__close');
 
-console.log(foo, bar);
+  var expanded = false;
 
-foo = 1;
-console.log('up');
+  var open = function open() {
+
+    var tile = $(this).parent();
+
+    if (!expanded) {
+      tile.addClass('strips__strip--expanded');
+      // add delay to inner text
+      tileText.css('transition', 'all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)');
+      stripClose.addClass('strip__close--show');
+      stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
+      expanded = true;
+    }
+  };
+
+  var close = function close() {
+    if (expanded) {
+      tile.removeClass('strips__strip--expanded');
+      // remove delay from inner text
+      tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
+      stripClose.removeClass('strip__close--show');
+      stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)');
+      expanded = false;
+    }
+  };
+
+  var bindActions = function bindActions() {
+    tileLink.on('click', open);
+    stripClose.on('click', close);
+  };
+
+  var init = function init() {
+    bindActions();
+  };
+
+  return {
+    init: init
+  };
+}();
+
+Expand.init();
